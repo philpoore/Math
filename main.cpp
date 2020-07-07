0d4a5d984d25dbf5f858dcea09dce7c124e73135
phil <phil.j.poore@gmail.com>
2020-07-07 21:59:38 +0100

    feat: read files as inputs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <limits>
@@ -11,6 +12,7 @@ using namespace std;
 
 Interp interp;
 
+// Take an input string and run it
 void run(string input) {
     Lexer lexer(input);
     auto tokens = lexer.tokenize();
@@ -34,15 +36,8 @@ void run(string input) {
     // cout << setprecision(16) << value << endl;
 }
 
-int main(int argc, char** argv) {
-    bool repl = argc == 1;
-
-    if (!repl) {
-        string input = string(argv[1]);
-        run(input);
-        exit(0);
-    }
-
+// Run a REPL in a loop, quit with "quit".
+void runRepl() {
     while (true) {
         string input;
         cout << "> ";
@@ -54,4 +49,49 @@ int main(int argc, char** argv) {
 
         run(input);
     }
+}
+
+// Read a file compleatly and return it as one big string.
+// @TODO: this is super silly with large files!
+string readFile(string filename) {
+    ifstream is(filename);
+    if (!is) {
+        cerr << "Runtime Error: Coudln't open file " << filename;
+        cerr << " for reading" << endl;
+        exit(1);
+    }
+    return string((std::istreambuf_iterator<char>(is)),
+                  std::istreambuf_iterator<char>());
+    ;
+}
+
+int main(int argc, char** argv) {
+    bool repl = argc == 1;
+
+    if (repl) {
+        runRepl();
+    }
+
+    string input;
+    string firstArg = string(argv[1]);
+
+    if (firstArg == "-f") {
+        // Use a file as input.
+        // E.g "./math -f examples/example-1.math"
+        if (argc < 3) {
+            cerr << "Runtime Error: Too few arguments\n";
+            cerr << "-f flag missing filename" << endl;
+            exit(1);
+        }
+        string filename = string(argv[2]);
+        // Read file for input
+        input = readFile(filename);
+    } else {
+        // Default to running from firstArg.
+        // E.g "./math 10+10"
+        input = string(firstArg);
+    }
+
+    run(input);
+    exit(0);
 }
